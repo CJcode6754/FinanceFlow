477dda6a030086308e86b8456a5b71a758ff1381
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-20 08:34:42 +0800

    feat: add header component

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,10 +2,6 @@
     <main>
         <x-sidebar-layout/>
 
-        <div class="p-4 sm:ml-64">
-            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                Hello
-            </div>
-        </div>
+        <x-header/>
     </main>
 </x-app-layout>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<header class="p-4 sm:ml-64">
+    <div class="p-4">
+        <div class="flex items-center justify-end gap-8 px-8">
+            <div class="border border-gray-200 rounded-lg py-1 px-3">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search" name="search" id="search" class="border-none">
+            </div>
+            <i class="fa-regular fa-bell text-lg"></i>
+            <div class="flex items-center gap-4">
+                <i class="fa-solid fa-user bg-gray-500 p-4 rounded-full"></i>
+                <div>
+                    <h3 class="text-base font-medium">Ceejay Ibabiosa</h3>
+                    <p class="text-sm font-light">example@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
