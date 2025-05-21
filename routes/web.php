0e5c06e3a9e186b9fdf8cde9a9cbf289d28add55
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-21 14:49:57 +0800

    refactor: add auth controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
 Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
