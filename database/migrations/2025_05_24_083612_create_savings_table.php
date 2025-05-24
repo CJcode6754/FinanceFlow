f1368aa366c005c846c8d32becb5d2c7a99cbe01
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-24 17:11:28 +0800

    create all neccessary migration

---
+++ database/migrations/2025_05_24_083612_create_savings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('savings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name');
+            $table->bigInteger('target_amount');
+            $table->bigInteger('current_amount');
+            $table->date('deadline');
+            $table->text('note');
+            $table->timestamps();
+        });
+
+        Schema::create('savings_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('saving_id')->constrained('savings');
+            $table->bigInteger('amount');
+            $table->enum('type', ['deposit', 'withdraw']);
+            $table->date('date');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('savings');
+        Schema::dropIfExists('savings_transactions');
+    }
+};
